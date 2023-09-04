1b064f31d6a2e13d1bcef9926c8f92dc9663332b
sophieewu <49039676+sophieewu@users.noreply.github.com>
2023-09-04 14:15:32 -0400

    Add files via upload

---
+++ if_ex6.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string weather;
+
+    // Prompt user to enter the weather condition today
+    cout << "How is the weather? <rain/snow/sunny/fog/cloudy>: ";
+    // Read the weather condition given by user
+    cin >> weather;
+
+    // Provide advice to user based on the weather condition
+    if (weather == "rain")
+        cout << "Take your umbrella" << endl;
+    else if (weather == "snow")
+        cout << "Take your scarf" << endl;
+    else if (weather == "sunny")
+        cout << "Take your sunglasses" << endl;
+    else
+        cout << "No special advice for you" << endl;
+  
+    return 0;
+}

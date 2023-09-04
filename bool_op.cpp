16485194f8b57555fc7cf6a73d1e7e13656c5d32
sophieewu <49039676+sophieewu@users.noreply.github.com>
2023-09-04 14:10:27 -0400

    Update bool_op.cpp

--- bool_op.cpp
+++ bool_op.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 int main(){
-  string light = "green";
-  int x = 3, y = 5;
-  cout << "x == 5: " << (x == 5) << endl;
-  cout << "light == \"green\": " << (light == "green") << endl;
-  cout << "x != y: " << (x != y) << endl;
-  cout << "light != \"green\": " << (light != "green") << endl;
-  cout << "x < y: " << (x < y) << endl;
-  cout << "x <= 5: " << (x <= 5) << endl;
-  cout << "3 > y: " << (3 > y) << endl;
-  cout << "3 >= y: " << (3 >= y) << endl;
+    string light = "green";
+    int x = 3, y = 5;
+    cout << "x == 5: " << (x == 5) << endl;
+    cout << "light == \"green\": " << (light == "green") << endl;
+    cout << "x != y: " << (x != y) << endl;
+    cout << "light != \"green\": " << (light != "green") << endl;
+    cout << "x < y: " << (x < y) << endl;
+    cout << "x <= 5: " << (x <= 5) << endl;
+    cout << "3 > y: " << (3 > y) << endl;
+    cout << "3 >= y: " << (3 >= y) << endl;
 
-  return 0;
+    return 0;
 }

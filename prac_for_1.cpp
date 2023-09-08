24a08a6178e8e02b5d98e5f3f2f696d6365f90b8
sophieewu <49039676+sophieewu@users.noreply.github.com>
2023-09-08 13:18:37 -0400

    Update prac_for_1.cpp

--- prac_for_1.cpp
+++ prac_for_1.cpp
@@ -2,22 +2,35 @@
 using namespace std;
 
 int main(){
-	int n = 0;
-        double n1, n2;
+	int n = 0, correct = 0;
+    double n1, n2;
+	
 	while (n < 5){
 		cout << "Give me a positive integer that is greater than 4: ";
 		cin >> n;
 	}
 
 	for (int i = 1; i <= n; ++i){
-		cout << "Give me two numbers: ";
+		cout << "I will ask you a question. Please give me two numbers: ";
 		cin >> n1 >> n2;
-		if (i % 3 == 1)
-			cout << n1 << " + " << n2 << " = " << n1 + n2 << endl;
-		else if (i % 3 == 2)
-			cout << n1 << " - " << n2 << " = " << n1 - n2 << endl;
-		else cout << n1 << " * " << n2 << " = " << n1 * n2 << endl;
+		double ans, key;
+		if (i % 3 == 1){
+			cout << n1 << " + " << n2 << " = ";
+			cin >> ans;
+			key = n1 + n2;
+		} else if (i % 3 == 2) {
+			cout << n1 << " - " << n2 << " = ";
+            cin >> ans;
+            key = n1 - n2;
+		} else {
+			cout << n1 << " * " << n2 << " = ";
+            cin >> ans;
+            key = n1 * n2;
+		}
+		if (ans != key) cout << "Wrong! The answer is " << key << endl;
+        else ++correct;
 	}
+	cout << "You got " << correct << " correct, " << n - correct << " wrong." << endl;
 
 	return 0;
 }

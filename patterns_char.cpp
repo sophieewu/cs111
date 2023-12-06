8e80d99cb5e22f9079755faffb1665ce54ddca4d
sophieewu <49039676+sophieewu@users.noreply.github.com>
2023-12-06 13:11:04 -0500

    Update patterns_char.cpp

--- patterns_char.cpp
+++ patterns_char.cpp
@@ -7,6 +7,7 @@ int main(){
 	srand(time(0));
 	int n = rand() % (7 - 3 + 1) + 3;
 
+	// version 1
 	for (int i = 0; i < n; ++i){
 		char c = 'a' + i;
 		for (int j = 0; j < n; ++j){
@@ -26,6 +27,33 @@ int main(){
 		}
 		cout << endl;
 	}
+
+	cout << endl;
+	// version 2
+	char letter = 'a';
+	for (int i = 0; i < n; ++i){;
+                for (int j = 0; j < n; ++j){
+                        if (j <= i) cout << letter;
+                        else cout << "*";
+                }
+                cout << endl;
+		++letter;
+        }
+
+        cout << endl;
+
+        for (int i = 0; i < n; ++i){
+		letter = 'a';
+                for (int j = 0; j < n; ++j){
+                        if (j <= i) {
+				cout << letter;
+				++letter;
+			}
+                        else cout << "*";
+                }
+                cout << endl;
+        }
+
 
 	return 0;
 }

2c78c70ef8e347aea82eec8c53fd3f6ad2cd1e39
sophieewu <49039676+sophieewu@users.noreply.github.com>
2023-12-28 18:03:32 -0500

    Update cin_ex2.cpp

--- cin_ex2.cpp
+++ cin_ex2.cpp
@@ -13,7 +13,7 @@ int main(){
     // Prompt user to provide luckyNum
     cout << "What's your lucky number?";
 
-    //Read the value and store the it in luckyNum
+    //Read the value and store it in luckyNum
     cin >> luckyNum;
   
     return 0;
